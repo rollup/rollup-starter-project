ef380b0393022778c47354b5e32e58331ac831bc
Dan Abramov <dan.abramov@gmail.com>
2016-02-05 20:37:55 +0000

    Reproduce the issue with jsnext:main and Lodash for Redux

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,8 @@
 import babel from 'rollup-plugin-babel';
+import npm from 'rollup-plugin-npm';
 
 export default {
   entry: 'lib/index.js',
   sourceMap: true,
-  plugins: [babel()]
+  plugins: [babel(), npm({ jsnext: true })]
 };
