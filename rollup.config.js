63d62e25ca0f92cb97234ddb5ed897758c5267e6
Paul Falgout <paul@otterball.com>
2017-05-04 22:46:21 +0900

    Prevent production build from instrumenting files
    Resolves #25

--- rollup.config.js
+++ rollup.config.js
@@ -5,14 +5,19 @@ import istanbul from 'rollup-plugin-istanbul';
 let pkg = require('./package.json');
 let external = Object.keys(pkg.dependencies);
 
+let plugins = [
+  babel(babelrc()),
+];
+
+if (process.env.BUILD !== 'production') {
+  plugins.push(istanbul({
+    exclude: ['test/**/*', 'node_modules/**/*']
+  }));
+}
+
 export default {
   entry: 'lib/index.js',
-  plugins: [
-    babel(babelrc()),
-    istanbul({
-      exclude: ['test/**/*', 'node_modules/**/*']
-    })
-  ],
+  plugins: plugins,
   external: external,
   targets: [
     {
